b292cba0a7f3d66dd0ca1673849d8173942d86cd
Evan Mattiza <emattiza@users.noreply.github.com>
2023-06-15 23:47:32 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import elmPlugin from "vite-plugin-elm";
 export default defineConfig({
   root: "public",
   server: { hmr: { clientPort: 443 } },
+  base: "/grazely/",
   plugins: [elmPlugin()],
 });
